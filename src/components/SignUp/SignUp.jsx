c38ea7cdaf227e45377813670a306ffb4aa7834c
Pozzi2002 <nik.stroganov.2019@mail.ru>
2025-03-24 20:30:41 +0300

    Realize fetch via handleSubmit in form

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,25 +1,48 @@
+import { useEffect, useState } from 'react';
 import Nav from '../partials/Nav/Nav';
-
+import classes from './signup.module.css'
+import brain from '../../assets/brain.png';
 function SignUp() {
+    const [response, setResponse] = useState(null);
+
+    const handleSubmit = (event) => {
+        // setResponse(event)
+        const body = event.currentTarget.elements;
+        event.preventDefault()
+        fetch('http://localhost:3000/session/new', {
+            mode: "cors",
+            method: 'POST',
+            enctype: 'application/x-www-form-urlencoded',
+            // body: {
+            //     firstname: body.firstname.value,
+            //     lastname: body.lastname.value,
+            //     nickname: body.nickname.value,
+            //     password: body.password.value,
+            //     confpassword: body.password.value 
+            // } 
+        }).then((response) => response.json()).then((response) => console.log(response))
+    };
+
     return (
         <>
         <Nav />
-        <main>
-            <form action="http://localhost:3000/posts" method="POST" enctype="multipart/form-data">
-
-                <h2 class="signUpLogo">Registration</h2>
-                <label for="firstName">First name</label>
-                 <input type="text" id="firstName" name="firstName" maxlength="20" required />
-                <label for="lastName">Last name</label>
-                 <input type="text" id="lastName" name="lastName" maxlength="20"  required />
-                <label for="nickName">Nickname</label>
-                 <input type="nickName" id="nickName" name="nickName" maxlength="20"  required />
-                <label for="password">Password</label>
-                 <input type="password" id="password" name="password" maxlength="25" required />
-                <label for="confPassword">Confirm password</label>
-                 <input type="password" id="confPassword" name="confPassword" minlength="8" maxlength="25" required />
+        <main className={classes.mainSignUp}>
+            <form onSubmit={handleSubmit}>
+                <h2>Registration</h2>
+                <label htmlFor="firstname">First name</label>
+                 <input type="text" id="firstname" name="firstname" maxLength="20" required />
+                <label htmlFor="lastname">Last name</label>
+                 <input type="text" id="lastname" name="lastname" maxLength="20"  required />
+                <label htmlFor="nickname">Nickname</label>
+                 <input type="text" id="nickname" name="nickname" maxLength="20"  required />
+                <label htmlFor="password">Password</label>
+                 <input type="password" id="password" name="password" maxLength="25" required />
+                <label htmlFor="confpassword">Confirm password</label>
+                 <input type="password" id="confpassword" name="confpassword" minLength="8" maxLength="25" required />
                 <button type="submit">Sign up</button> 
             </form>
+            <h3 className={classes.textBeforeBrain}>Hi, registry there</h3>
+            <img src={brain} className={classes.brain} />
         </main>
         </>
     )
