ac547eadca88b307e70e6e3a9702a7fcad4d4e5a
Pozzi2002 <nik.stroganov.2019@mail.ru>
2025-03-29 20:34:37 +0300

    Add little style and delete unnecessary files

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
-import './index.css'
 import App from './components/App/App'
 
 
